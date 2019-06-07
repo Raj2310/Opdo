60381c30a59ae01ae443285de20426c630d89753
rajs <rajs@nrifintech.com>
2019-06-07 15:55:18 +0530

    Added dropdown for label

--- src/Components/TaskCreator/index.jsx
+++ src/Components/TaskCreator/index.jsx
@@ -1,44 +1,72 @@
-import React, { Component } from 'react'
-import { Button, Header, Icon, Modal, Input } from 'semantic-ui-react'
+import React, { Component } from "react";
+import {
+  Button,
+  Header,
+  Icon,
+  Modal,
+  Input,
+  Dropdown
+} from "semantic-ui-react";
 
 export default class TaskCreator extends Component {
-    state = { modalOpen: false,
-                taskText:'' }
+  state = {
+    modalOpen: false,
+    taskText: "",
+    labels: [
+      { key: "Read Book", text: "Read Book", value: "Read Book" },
+      { key: "Coding", text: "Coding", value: "Coding" }
+    ]
+  };
 
-    handleOpen = () => this.setState({ modalOpen: true })
+  handleOpen = () => this.setState({ modalOpen: true });
 
-    handleClose = () => this.setState({ modalOpen: false })
+  handleClose = () => this.setState({ modalOpen: false });
 
-    handleChange = (ev,data) =>{
-        this.setState({ taskText: data.value})
-    }
+  handleChange = (ev, data) => {
+    this.setState({ taskText: data.value });
+  };
 
-    handleClick =()=>{
-        this.props.handleClick(this.state.taskText)
-        this.setState({
-            modalOpen: false
-        })
-    }
-    render() {
-        return (
-            <Modal
-                trigger={<Button onClick={this.handleOpen}
-                        floated='right'>Create</Button>}
-                open={this.state.modalOpen}
-                onClose={this.handleClose}
-                size='small'
-            >
-                <Header icon='edit' content='New Task' />
-                <Modal.Content>
-                    <Input onChange={this.handleChange} placeholder='Learn Rx...' />
-                </Modal.Content>
-                <Modal.Actions>
-                    <Button color='green' onClick={this.handleClick} inverted
-                        disabled={!this.state.taskText}>
-                        <Icon name='checkmark' /> Got it
+  handleClick = () => {
+    this.props.handleClick(this.state.taskText);
+    this.setState({
+      modalOpen: false
+    });
+  };
+  render() {
+    return (
+      <Modal
+        trigger={
+          <Button onClick={this.handleOpen} floated="right">
+            Create
           </Button>
-                </Modal.Actions>
-            </Modal>
-        )
-    }
+        }
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        size="small"
+      >
+        <Header icon="edit" content="New Task" />
+        <Modal.Content>
+          <Input onChange={this.handleChange} placeholder="Learn Rx..." />
+          <Dropdown
+            placeholder="Labels"
+            fluid
+            multiple
+            search
+            selection
+            options={this.state.labels}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            color="green"
+            onClick={this.handleClick}
+            inverted
+            disabled={!this.state.taskText}
+          >
+            <Icon name="checkmark" /> Got it
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    );
+  }
 }
